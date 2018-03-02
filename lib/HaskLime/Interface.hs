9923f06d5b2ad5ee68cedf2dcfdfbd4305e06eba
Ole <ole@vprsm.de>
2018-03-02 19:41:33 +0000

    Added names of foreign wrappers for Interface instances

--- lib/HaskLime/Interface.hs
+++ lib/HaskLime/Interface.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE TypeOperators     #-}
 
@@ -13,6 +14,7 @@ module HaskLime.Interface (
 ) where
 
 import           Data.Aeson             hiding (fromJSON)
+import           Data.Tagged
 
 import qualified Data.ByteString        as ByteString
 import qualified Data.ByteString.Lazy   as ByteString (toStrict)
@@ -38,67 +40,93 @@ class Interface a where
     default toC :: a ~ CType a => a -> IO (CType a)
     toC = pure
 
+    -- | Name of the wrapper on foreign side
+    foreignWrapperName :: Tagged a ByteString.ByteString
+
 instance Interface () where
+    foreignWrapperName = Tagged "Unit"
 
-instance Interface (Ptr a)
+instance Interface (Ptr a) where
+    foreignWrapperName = Tagged "Pointer"
 
-instance Interface CBool
+instance Interface CBool where
+    foreignWrapperName = Tagged "Bool"
 
-instance Interface CClock
+-- instance Interface CClock
 
-instance Interface CDouble
+instance Interface CDouble where
+    foreignWrapperName = Tagged "Double"
 
-instance Interface CFile
+-- instance Interface CFile
 
-instance Interface CFloat
+instance Interface CFloat where
+    foreignWrapperName = Tagged "Float"
 
-instance Interface CFpos
+-- instance Interface CFpos
 
-instance Interface CInt
+instance Interface CInt where
+    foreignWrapperName = Tagged "Int"
 
-instance Interface CIntMax
+-- instance Interface CIntMax
 
-instance Interface CIntPtr
+-- instance Interface CIntPtr
 
-instance Interface CJmpBuf
+-- instance Interface CJmpBuf
 
-instance Interface CLLong
+instance Interface CLLong where
+    foreignWrapperName = Tagged "LongLong"
 
-instance Interface CLong
+instance Interface CLong where
+    foreignWrapperName = Tagged "Long"
 
-instance Interface CPtrdiff
+-- instance Interface CPtrdiff
 
-instance Interface CSChar
+-- instance Interface CSChar
 
-instance Interface CShort
+instance Interface CShort where
+    foreignWrapperName = Tagged "Short"
 
-instance Interface CSigAtomic
+-- instance Interface CSigAtomic
 
-instance Interface CSize
+instance Interface CSize where
+    foreignWrapperName = Tagged "Size"
 
-instance Interface CSUSeconds
+-- instance Interface CSUSeconds
 
-instance Interface CTime
+-- instance Interface CTime
 
-instance Interface CUChar
+-- instance Interface CUChar
 
-instance Interface CUInt
+instance Interface CUInt where
+    foreignWrapperName = Tagged "UInt"
 
-instance Interface CUIntMax
+-- instance Interface CUIntMax
 
-instance Interface CUIntPtr
+-- instance Interface CUIntPtr
 
-instance Interface CULLong
+instance Interface CULLong where
+    foreignWrapperName = Tagged "ULongLong"
 
-instance Interface CULong
+instance Interface CULong where
+    foreignWrapperName = Tagged "ULong"
 
-instance Interface CUSeconds
+-- instance Interface CUSeconds
 
-instance Interface CUShort
+instance Interface CUShort where
+    foreignWrapperName = Tagged "UShort"
 
-instance Interface CWchar
+-- instance Interface CWchar
 
-instance Interface Bool
+instance Interface Bool where
+    type CType Bool = CBool
+
+    toC False = pure (CBool 0)
+    toC True  = pure (CBool 1)
+
+    fromC (CBool 0) = pure False
+    fromC _         = pure True
+
+    foreignWrapperName = Tagged "Bool"
 
 instance Interface ByteString.ByteString where
     type CType ByteString.ByteString = CString
@@ -115,6 +143,8 @@ instance Interface ByteString.ByteString where
         where
             valueLength = ByteString.length value
 
+    foreignWrapperName = Tagged "ByteString"
+
 -- | Transported as JSON-encoded C string
 newtype JSON a = JSON {fromJSON :: a}
 
@@ -126,6 +156,8 @@ instance (ToJSON a, FromJSON a) => Interface (JSON a) where
 
     toC = toC . ByteString.toStrict . encode . fromJSON
 
+    foreignWrapperName = Tagged "JSON"
+
 -- | Transportered as stable pointer
 newtype Ref a = Ref {deRef :: a}
 
@@ -135,3 +167,5 @@ instance Interface (Ref a) where
     fromC x = Ref <$> deRefStablePtr x
 
     toC (Ref x) = newStablePtr x
+
+    foreignWrapperName = Tagged "Ref"
